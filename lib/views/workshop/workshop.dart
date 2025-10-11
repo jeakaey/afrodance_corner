e21a4e0d0bd5eb9967199e3c766b48c1b1ca330c
jeakaey <tiayokarelle@gmail.com>
2025-10-11 23:20:27 +0200

    chore: modify workshop attributes

--- lib/views/workshop/workshop.dart
+++ lib/views/workshop/workshop.dart
@@ -2,19 +2,16 @@
 class Workshop {
   final String theme;
   final String date;
+  final String dateLine;
   final String time;
   final String place;
-  final String coach;
-  final String dj;
-  final double cost;
+ 
 
 const Workshop({
   required this.theme,
   required this.date,
   required this.time,
   required this.place,
-  required this.coach,
-  required this.dj,
- required this.cost,
+  required this.dateLine,
 });
 }
